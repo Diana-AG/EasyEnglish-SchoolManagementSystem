c426ca5bd48c4e0b3fbabdb6530a54b3d089a77f
Diana Georgieva <deedee.ag@gmail.com>
2022-03-30 10:19:49 +0300

    Add Levels and CourseTypes Controllers and Views

---
+++ Web/EasyEnglish.Web/Areas/Administration/Controllers/CourseTypesController.cs
@@ -0,0 +1,166 @@
+﻿namespace EasyEnglish.Web.Areas.Administration.Controllers
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using EasyEnglish.Data;
+    using EasyEnglish.Data.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Microsoft.EntityFrameworkCore;
+
+    [Area("Administration")]
+    public class CourseTypesController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CourseTypesController(ApplicationDbContext context)
+        {
+            this.dbContext = context;
+        }
+
+        // GET: Administration/CourseTypes
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = this.dbContext.CourseTypes.Include(c => c.Language).Include(c => c.Level);
+            return this.View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Administration/CourseTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return this.NotFound();
+            }
+
+            var courseType = await this.dbContext.CourseTypes
+                .Include(c => c.Language)
+                .Include(c => c.Level)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (courseType == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(courseType);
+        }
+
+        // GET: Administration/CourseTypes/Create
+        public IActionResult Create()
+        {
+            this.ViewData["LanguageId"] = new SelectList(this.dbContext.Languages, "Id", "Name");
+            this.ViewData["LevelId"] = new SelectList(this.dbContext.Levels, "Id", "Name");
+            return this.View();
+        }
+
+        // POST: Administration/CourseTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("LanguageId,LevelId,Description,IsDeleted,DeletedOn,Id,CreatedOn,ModifiedOn")] CourseType courseType)
+        {
+            if (this.ModelState.IsValid)
+            {
+                this.dbContext.Add(courseType);
+                await this.dbContext.SaveChangesAsync();
+                return this.RedirectToAction(nameof(this.Index));
+            }
+            this.ViewData["LanguageId"] = new SelectList(this.dbContext.Languages, "Id", "Name", courseType.LanguageId);
+            this.ViewData["LevelId"] = new SelectList(this.dbContext.Levels, "Id", "Name", courseType.LevelId);
+            return this.View(courseType);
+        }
+
+        // GET: Administration/CourseTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return this.NotFound();
+            }
+
+            var courseType = await this.dbContext.CourseTypes.FindAsync(id);
+            if (courseType == null)
+            {
+                return this.NotFound();
+            }
+            this.ViewData["LanguageId"] = new SelectList(this.dbContext.Languages, "Id", "Name", courseType.LanguageId);
+            this.ViewData["LevelId"] = new SelectList(this.dbContext.Levels, "Id", "Name", courseType.LevelId);
+            return this.View(courseType);
+        }
+
+        // POST: Administration/CourseTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("LanguageId,LevelId,Description,IsDeleted,DeletedOn,Id,CreatedOn,ModifiedOn")] CourseType courseType)
+        {
+            if (id != courseType.Id)
+            {
+                return this.NotFound();
+            }
+
+            if (this.ModelState.IsValid)
+            {
+                try
+                {
+                    this.dbContext.Update(courseType);
+                    await this.dbContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!this.CourseTypeExists(courseType.Id))
+                    {
+                        return this.NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return this.RedirectToAction(nameof(this.Index));
+            }
+            this.ViewData["LanguageId"] = new SelectList(this.dbContext.Languages, "Id", "Name", courseType.LanguageId);
+            this.ViewData["LevelId"] = new SelectList(this.dbContext.Levels, "Id", "Name", courseType.LevelId);
+            return this.View(courseType);
+        }
+
+        // GET: Administration/CourseTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return this.NotFound();
+            }
+
+            var courseType = await this.dbContext.CourseTypes
+                .Include(c => c.Language)
+                .Include(c => c.Level)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (courseType == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(courseType);
+        }
+
+        // POST: Administration/CourseTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var courseType = await this.dbContext.CourseTypes.FindAsync(id);
+            this.dbContext.CourseTypes.Remove(courseType);
+            await this.dbContext.SaveChangesAsync();
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        private bool CourseTypeExists(int id)
+        {
+            return this.dbContext.CourseTypes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Web/EasyEnglish.Web/Areas/Administration/Controllers/LevelsController.cs
@@ -0,0 +1,151 @@
+﻿namespace EasyEnglish.Web.Areas.Administration.Controllers
+{
+    using EasyEnglish.Data;
+    using EasyEnglish.Data.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Area("Administration")]
+    public class LevelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LevelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Administration/Levels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Levels.ToListAsync());
+        }
+
+        // GET: Administration/Levels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var level = await _context.Levels
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (level == null)
+            {
+                return NotFound();
+            }
+
+            return View(level);
+        }
+
+        // GET: Administration/Levels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Administration/Levels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,IsDeleted,DeletedOn,Id,CreatedOn,ModifiedOn")] Level level)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(level);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(level);
+        }
+
+        // GET: Administration/Levels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var level = await _context.Levels.FindAsync(id);
+            if (level == null)
+            {
+                return NotFound();
+            }
+            return View(level);
+        }
+
+        // POST: Administration/Levels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,IsDeleted,DeletedOn,Id,CreatedOn,ModifiedOn")] Level level)
+        {
+            if (id != level.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(level);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LevelExists(level.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(level);
+        }
+
+        // GET: Administration/Levels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var level = await _context.Levels
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (level == null)
+            {
+                return NotFound();
+            }
+
+            return View(level);
+        }
+
+        // POST: Administration/Levels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var level = await _context.Levels.FindAsync(id);
+            _context.Levels.Remove(level);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LevelExists(int id)
+        {
+            return _context.Levels.Any(e => e.Id == id);
+        }
+    }
+}
